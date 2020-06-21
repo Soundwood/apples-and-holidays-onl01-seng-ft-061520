a542e26a1b78f6db7a3bc2a2f49b98bfef78ec61
Ben Hogan <j.benhogan@gmail.com>
2020-06-21 15:39:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holidays, supplies|
+      if supplies == "BBQ"
+        puts holidays
+      end
+    end
+  end
 end
 
 
