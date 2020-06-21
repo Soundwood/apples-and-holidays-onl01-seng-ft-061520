fe7fba9ceeeeecc8ceb68d5851b191f16dbb2cfc
Ben Hogan <j.benhogan@gmail.com>
2020-06-21 14:35:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_supplies.each do |season, holiday|
     if season == "winter"
-      season[:holiday] << "Balloons"
+      season[holiday] << "Balloons"
     end
   end
 
